ec55ecf297af482428cc2911d1a180f21fd60c7e
yunkyung-Hwang <dudhkd98@naver.com>
2022-09-03 17:36:58 +0900

    [ADD]: font

---
+++ LifeLibrary/LifeLibrary/Global/Extension/UIFont+.swift
@@ -0,0 +1,23 @@
+//
+//  UIFont+.swift
+//  LifeLibrary
+//
+//  Created by 황윤경 on 2022/09/03.
+//
+
+import UIKit
+
+extension UIFont {
+    // Basic
+    class func PretendardBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-Bold", size: size)!
+    }
+
+    class func PretendardRegular(size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-Regular", size: size)!
+    }
+    
+    class func PretendardLight(size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-Light", size: size)!
+    }
+}
