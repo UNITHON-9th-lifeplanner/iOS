4ede33d693621a610c5948abf61436e89fe04678
김주영 <joo-young.kim@navercorp.com>
2022-09-03 14:04:20 +0900

    Revert "[FEAT]: 프로젝트 초기 설정"
    This reverts commit e6775b3c0be40d28998e4fc0f765b2a9320a7809.

---
+++ LifeLibrary/LifeLibrary/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  LifeLibrary
+//
+//  Created by USER on 2022/09/03.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
