b1b3512ff39e7cc4db8d67040c82ed1d9ead4b25
김주영 <joo-young.kim@navercorp.com>
2022-09-03 16:02:28 +0900

    # [FEAT] : 스플래시, 로그인, 회원가입 베이스화면 작성

---
+++ LifeLibrary/LifeLibrary/Screen/User/Login/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  LifeLibrary
+//
+//  Created by USER on 2022/09/03.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// Touch Event
+extension LoginViewController {
+    @IBAction func onTouchLoginButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onTouchSignUpButton(_ sender: Any) {
+        
+    }
+}
---
+++ LifeLibrary/LifeLibrary/Screen/User/SignUp/SignUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpViewController.swift
+//  LifeLibrary
+//
+//  Created by USER on 2022/09/03.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
