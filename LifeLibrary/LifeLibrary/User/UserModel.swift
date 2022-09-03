d7b5b6ce8e4f92e516b9e7e484f55a8d3b564d48
김주영 <joo-young.kim@navercorp.com>
2022-09-03 09:55:23 +0900

    UserModel 기본구조

--- LifeLibrary/LifeLibrary/User/UserModel.swift
+++ LifeLibrary/LifeLibrary/User/UserModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import SwiftyJSON
+
+class UserModel: NSObject, Codable {
+    var email: String
+    var name: String
+    var age: Int
+    var accessToken: String
+    
+    init(email: String, name: String, age: Int, accessToken: String) {
+        self.email = email
+        self.name = name
+        self.age = age
+        self.accessToken = accessToken
+    }
+    
+    convenience init?(json: JSON) {
+        guard let email = json["email"].string,
+              let name = json["name"].string,
+              let age = json["age"].int,
+              let accessToken = json["accessToken"].string else { return nil }
+        
+        self.init(email: email, name: name, age: age, accessToken: accessToken)
+    }
+}
