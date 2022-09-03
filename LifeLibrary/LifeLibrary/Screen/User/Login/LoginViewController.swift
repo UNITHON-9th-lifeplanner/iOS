08f52ad861aeaa73d2f8189188987f4677aa65e5
김주영 <joo-young.kim@navercorp.com>
2022-09-03 16:16:13 +0900

    [CHORE] : 온보딩으로 네이밍 변경, 로그인 회원가입 화면 연결

--- LifeLibrary/LifeLibrary/Screen/User/Login/LoginViewController.swift
+++ LifeLibrary/LifeLibrary/Screen/User/Login/LoginViewController.swift
@@ -9,21 +9,7 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    @IBOutlet weak var loginButton: UIButton!
-    @IBOutlet weak var signupButton: UIButton!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-}
-
-// Touch Event
-extension LoginViewController {
-    @IBAction func onTouchLoginButton(_ sender: Any) {
-        
-    }
-    
-    @IBAction func onTouchSignUpButton(_ sender: Any) {
-        
     }
 }
